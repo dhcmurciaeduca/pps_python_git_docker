1d0666f304530a807871e11f8118d4daf3ddaa79
dhcmurciaeduca <dhcmurciaeduca@gmail.com>
2024-01-30 19:51:49 +0100

    rama experto_web usando flask

--- app.py
+++ app.py
@@ -1,3 +1,23 @@
 # app.py
+#Importar en app.py la función frotar de bayeta.py
+from bayeta import frotar
+#Importar flask
+from flask import Flask,jsonify
 
-print("Hola, mundo")
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return frotar(5)
+
+@app.route('/frotar/<int:n_frases>', methods=['GET'])
+def obtener_frases(n_frases):
+    frases = frotar(n_frases)
+    return jsonify({'frases': frases})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+#print("Hola, mundo")
+#print(frotar())
---
+++ bayeta.py
@@ -0,0 +1,2 @@
+def frotar(n_frases: int = 1) -> list():
+    return ["Hola mundo" for _ in range(n_frases)]
