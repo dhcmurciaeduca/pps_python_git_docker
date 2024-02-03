ba9c737ab0e3b2e40544456fa2d17f77ca776337
dhcmurciaeduca <11233535@alu.murciaeduca.com>
2024-02-03 19:51:47 +0100

    rama experto python

--- bayeta.py
+++ bayeta.py
@@ -1,2 +1,14 @@
-def frotar(n_frases: int = 1) -> list():
-    return ["Hola mundo" for _ in range(n_frases)]
+import random
+
+def frotar(n_frases: int = 1) -> list:
+    with open('pps_python_git_docker/frases.txt', 'r') as archivo:
+        lista_de_frases = archivo.readlines()
+    
+    
+    lista_de_frases = [frase.strip() for frase in lista_de_frases]
+    
+    
+    frases_elegidas = random.sample(lista_de_frases, n_frases)
+    
+    return frases_elegidas
+
